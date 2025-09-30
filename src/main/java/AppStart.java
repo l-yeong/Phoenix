a77ee4fd662fd2e0c17f91ef26b0d32f593eb856
l-yeong <yahn1204@gmail.com>
2025-09-30 12:22:49 +0900

    Initial commit

---
+++ src/main/java/AppStart.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class AppStart {
+    public static void main(String[] args) {
+        SpringApplication.run(AppStart.class);
+    }//main end
+} // class end
