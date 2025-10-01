42a83729dd774cb0203786511834759f6a5cc40d
LEE-GYEO-RE <rufp9891@gmail.com>
2025-10-01 17:02:57 +0900

    251001_MembersService_로그인 구현 중

--- src/main/java/model/mapper/MembersMapper.java
+++ src/main/java/model/mapper/MembersMapper.java
@@ -1,7 +1,20 @@
 package model.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import model.dto.MembersDto;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface MembersMapper {
+
+    // 회원가입 (일반회원만 - mid, password_hash, 이름, 전화번호, 생년월일, 이메일)
+    @Insert("insert into members(mid, password_hash, mname, mphone, birthdate, email) " +
+            "VALUES(#{mid}, #{password_hash}, #{mname}, #{mphone}, #{birthdate}, #{email})")
+    @Options(useGeneratedKeys = true, keyProperty = "mno")
+    int signUp(MembersDto member);
+
+    // 로그인 (아이디 + 비밀번호 + 활성 회원 상태)
+    @Select("SELECT * FROM members WHERE mid = #{mid} AND password_hash = #{password_hash} AND status = 'active'")
+    MembersDto login(@Param("mid") String mid, @Param("password_hash") String password_hash);
+
+
 }//inter end
