32e25495fb5c0cacf97e0001e6d79a74637a2756
msh-94 <alstjdgh2003@naver.com>
2025-10-06 10:27:09 +0900

    251006_ThreadPoolConfig_교환신청용 쓰레드풀 설정값 추가

--- src/main/java/phoenix/service/ReservationExchangesService.java
+++ src/main/java/phoenix/service/ReservationExchangesService.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ReservationExchangesService {
-    private final ReservationExchangeMapper reservationexchangeMapper;
-
+    private final ReservationExchangesMapper reservationexchangeMapper;
 }//func end
