273ef497314b36dc51cf3eede35d3b736f9cebb5
l-yeong <yahn10@naver.com>
2025-10-11 16:25:08 +0900

    251011_main.jsx_QR코드이미지테스트

--- src/main/reactapp/src/main.jsx
+++ src/main/reactapp/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+//
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+//2. index.html(SPA) 에서 root 마크업 가져오기
+const root = document.querySelector('#root');
+
+//3. 가져온 root 마크업을  createRoot 함수의 매개변수로 전달한다
+const create = createRoot(root);
+
+import TicketQR from './features/tickets/TicketQR';
+create.render(
+      <TicketQR />
+);
