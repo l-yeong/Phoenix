c34d151100004f1477d442116d356a66e6ab0c8a
l-yeong <yahn10@naver.com>
2025-10-11 16:35:46 +0900

    251011_TicktsMapper_QR코드값출력만추가

--- src/main/java/phoenix/model/mapper/TicketsMapper.java
+++ src/main/java/phoenix/model/mapper/TicketsMapper.java
@@ -12,10 +12,10 @@ import java.util.Objects;
 @Mapper
 public interface TicketsMapper {
 
-    @Select("select t.tno,t.ticket_code,t.price as ticket_price,t.valid,r.rno,r.status " +
-    " as reservation_status,m.mno,m.mname,m.email,m.mphone,s.sno,s.seat_no,z.zname,z.price " +
-    " as zone_price from tickets t join reservations r on t.rno = r.rno join members m on r.mno = m.mno " +
-    " join seats s on r.sno = s.sno join zones z on s.zno = z.zno where m.mno=#{mno} order by t.tno ;")
+    @Select("SELECT t.ticket_code,t.price AS ticket_price,t.valid,r.status AS reservation_status, "+
+            " m.mname,m.email,m.mphone,s.seat_no,z.zname,z.price AS zone_price " +
+            " FROM tickets t JOIN reservations r ON t.rno = r.rno  JOIN members m ON r.mno = m.mno "+
+            " JOIN seats s ON r.sno = s.sno JOIN zones z ON s.zno = z.zno WHERE m.mno = #{mno} ORDER BY t.tno ")
     public List<Map<String, Object>> ticketPrint(int mno);
 
 }//inter end
