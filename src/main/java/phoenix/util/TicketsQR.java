136243a4b5aefc7f78b472c779298b684d2eacb7
l-yeong <yahn10@naver.com>
2025-10-11 16:28:54 +0900

    251011_TicketQR_QR코드헬퍼메소드추가

--- src/main/java/phoenix/util/TicketsQR.java
+++ src/main/java/phoenix/util/TicketsQR.java
@@ -1,36 +1,45 @@
 package phoenix.util;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
 import java.util.Map;
 
 public class TicketsQR {
 
     // [*] qr 생성
     public static byte[] TicketQrCode(Map<String, Object> TicketQrMap) {
-        ByteArrayOutputStream bout = null;
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
         try {
-            // 1️⃣ QR에 넣을 문자열 만들기
-            String qrData = TicketQrMap.toString(); // Map을 문자열로 변환 (원하는 형태로 변환 가능)
+            // Map 에서 JOSN 문자열 (한글)
+            // BitMatrix 생성
+            ObjectMapper mapper = new ObjectMapper();
+            String qrData = mapper.writeValueAsString(TicketQrMap);
 
-            // 2️⃣ BitMatrix 생성
-            MultiFormatWriter writer = new MultiFormatWriter();
-            BitMatrix matrix = writer.encode(qrData, BarcodeFormat.QR_CODE, 200, 200);
+            // QR UTF-8 설정
+            Map<EncodeHintType, Object>hints = new HashMap<>();
+            hints.put(EncodeHintType.CHARACTER_SET,"UTF-8"); // QR 코드 문자 인코딩
+            hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L); // QR 이미지 일부 가려져있을때 읽을수 있는 복원 레벨
 
-            // 3️⃣ ByteArrayOutputStream 생성
-            bout = new ByteArrayOutputStream();
+            // QR 생성
+            BitMatrix matrix = new MultiFormatWriter().encode(qrData,BarcodeFormat.QR_CODE,200, 200, hints);
 
-            // 4️⃣ BitMatrix를 PNG로 변환
+            // PNG로 변환
             MatrixToImageWriter.writeToStream(matrix, "PNG", bout);
+
+            return bout.toByteArray();
 
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        return bout.toByteArray();
-    }
+        }//catch end
+        return new byte[0];
+    }//func end
 
 }//class end
