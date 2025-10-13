9da81360723b3b33b517720595305bb1da294b5d
msh-94 <alstjdgh2003@naver.com>
2025-10-13 17:15:27 +0900

    251013_ReservationExchangesController_교환요청 등록 , 로그인한 회원한테 온 요청목록 전체조회 추가

--- src/main/java/phoenix/controller/ReservationExchangesController.java
+++ src/main/java/phoenix/controller/ReservationExchangesController.java
@@ -1,14 +1,43 @@
 package phoenix.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import phoenix.model.dto.ReservationExchangesDto;
+import phoenix.service.RedisService;
 import phoenix.service.ReservationExchangesService;
 
-@RequestMapping("")
+import java.util.List;
+
+@RequestMapping("/exchange")
 @RestController
 @RequiredArgsConstructor
 public class ReservationExchangesController {
     private final ReservationExchangesService reservationexchangesService;
+    private final RedisService redisService;
+
+    /**
+     * 교환요청 등록
+     *
+     * @param dto 요청 dto
+     * @return boolean 성공 : true , 실패 : false
+     */
+    @PostMapping("")
+    public ResponseEntity<?> saveRequest(ReservationExchangesDto dto){
+        boolean result = reservationexchangesService.requestChange(dto);
+        return ResponseEntity.ok(result);
+    }// func end
+
+    /**
+     * 로그인한 회원한테 온 요청목록 조회
+     *
+     * @param to_rno 로그인한 회원 얘매번호
+     * @return List<ReservationExchangesDto> 요청목록
+     */
+    @GetMapping("/findAll")
+    public ResponseEntity<List<ReservationExchangesDto>> getAllRequest(@RequestParam int to_rno){
+        List<ReservationExchangesDto> list = redisService.getAllRequest(to_rno);
+        return ResponseEntity.ok(list);
+    }// func end
 
 }//func end
