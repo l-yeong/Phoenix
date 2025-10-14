d3fd1b0bd091f5386c82346be1a76d39d557f4e1
l-yeong <yahn1204@gmail.com>
2025-10-14 18:12:22 +0900

    251014_TicketsMapper_리액트QR코드렌더링할조회추가

--- src/main/java/phoenix/model/mapper/TicketsMapper.java
+++ src/main/java/phoenix/model/mapper/TicketsMapper.java
@@ -18,14 +18,20 @@ public interface TicketsMapper {
 //    public List<Map<String, Object>> ticketPrint(int mno);
 
     // 예약 상태 확인
-    @Select("SELECT r.status AS reservation_status,z.price  AS seat_price " +
-            "FROM reservations r JOIN seats s ON r.sno = s.sno JOIN zones z ON s.zno = z.zno " +
-            "WHERE r.rno = #{rno}")
+    @Select("SELECT r.status AS reservation_status, z.price  AS seat_price, "+
+        " m.mname  AS mname,z.zname  AS zname, s.seat_no AS seat_no "+
+        " FROM reservations r JOIN seats s ON r.sno = s.sno JOIN zones z ON s.zno = z.zno "+
+        " JOIN members m ON r.mno = m.mno WHERE r.rno = #{rno} ")
     Map<String,Object> ticketPrint(@Param("rno") int rno);
 
     // 티켓 QR 이미지 추가
-    @Insert(" INSERT INTO tickets (rno, ticket_code, price, valid) VALUES (#{rno}, #{ticketCode}, #{price}, #{valid})")
+    @Insert(" INSERT INTO tickets (rno, ticket_code, price, valid) VALUES (#{rno}, #{ticket_code}, #{price}, #{valid}")
     @Options(useGeneratedKeys = true, keyProperty = "tno")
-    int insertTicket(TicketsDto dto);
+    int ticketWrite(TicketsDto dto);
+
+    // payload 목록 조회(프론트에서 qrcode 패키지 사용될것)
+    @Select("SELECT t.qr_payload FROM ticket t JOIN reservations r ON t.rno = r.rno JOIN members m ON r.mno = m.mno"
+            +"WHERE m.mno = #{mno} ORDER BY t.tno DESC")
+    List<String>findPayloads(@Param("mno")int mno);
 
 }//inter end
