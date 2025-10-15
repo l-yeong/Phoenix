d9ca91ef9f8e72f47612def549ff00d65615b9fd
l-yeong <yahn1204@gmail.com>
2025-10-15 14:57:46 +0900

    251015_Controller,Mapper,Service-프론트QR이미지생성테스트진행해야함

--- src/main/java/phoenix/configuration/RedisConfig.java
+++ src/main/java/phoenix/configuration/RedisConfig.java
@@ -1,11 +1,12 @@
 package phoenix.configuration;// 패키지명
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
