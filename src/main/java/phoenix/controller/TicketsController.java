90d1d038b711fde341a57ac90283a65e0db6538f
l-yeong <yahn1204@gmail.com>
2025-10-15 18:16:11 +0900

    251015_Controller,Mapper,Service_토큰인증된회원으로QR이미지생성완료,QR코드출력이 DB저장되어있는값으로출력중,QR코드 출력값용 R추가

--- src/main/java/phoenix/controller/TicketsController.java
+++ src/main/java/phoenix/controller/TicketsController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import phoenix.service.TicketsService;
 import java.util.List;
+import java.util.Map;
 
 
 @RequestMapping("/ticket")
@@ -13,18 +14,25 @@ import java.util.List;
 public class TicketsController {
     private final TicketsService ticketsService;
 
-    // (기존) 발급: reserved일 때 INSERT
+    //예약 rno가 'reserved' 일 때만 QR 문자열 생성하여 tickets에 테이블에 저장
     @PostMapping("/write")
     public ResponseEntity<?> ticketWrite(@RequestParam int rno) {
-        boolean ok = ticketsService.ticketWrite(rno);
-        return ok ? ResponseEntity.ok().build() : ResponseEntity.noContent().build();
+        boolean result = ticketsService.ticketWrite(rno);
+        return ResponseEntity.ok(result);
     }//func end
 
-    // 프론트가 쓰는 payload 조회 (qrcode.react 렌더링용)
+    // 프론트 QR코드 생성하는 payload 조회 (qrcode.react 렌더링용)
     @GetMapping("/payloads")
     public ResponseEntity<List<String>> payloads(@RequestParam int mno) {
-        List<String> list = ticketsService.getPayloadsByMno(mno);
-        return ResponseEntity.ok(list);
+        List<String> result = ticketsService.findPayloads(mno);
+        return ResponseEntity.ok(result);
+    }//func end
+
+    // QR코드 (이름/구역/좌석/사용여부)정보 출력
+    @GetMapping("/payloads/info")
+    public ResponseEntity<?>findPayloadsInfo(@RequestParam String ticket_code){
+        Map<String,Object> result = ticketsService.findPayloadsInfo(ticket_code);
+        return ResponseEntity.ok(result);
     }//func end
 
 }//func end
