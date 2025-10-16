5fb05e24609bd35b7b7b47cf9b0316949a2f0ce7
msh-94 <alstjdgh2003@naver.com>
2025-10-16 09:46:15 +0900

    251015_Mypage.jsx_예매내역 출력 작업중

--- src/main/reactapp/src/features/members/Mypage.jsx
+++ src/main/reactapp/src/features/members/Mypage.jsx
@@ -1,5 +1,10 @@
+import { useEffect, useState } from "react";
+
 export default function Mypage( props ){
-    // [1] 예매내역 전체조회
+    // 예매내역 상태
+    const [ reservations , setReservations ] = useState([]);
+        
+    // [1] 예매내역 전체조회 ( 회원번호 쿼리스트링으로 전달 )
     const reservePrint = async() => {
         
         try{
@@ -9,9 +14,33 @@ export default function Mypage( props ){
             console.log(e);
         }// try end
     }// func end
+
+    // [2] 컴포넌트 처음 렌더링 때 호출
+    useEffect( () => {
+        reservePrint();
+    },[]); 
+    // ( 예매내역에 경기 정보랑 경기날짜 시간 , 좌석 )
     return (
         <>
-        
+        <h2> 예매 내역 </h2>
+        {reservations.length == 0 ? (
+            <p> 예매 내역이 없습니다. </p>
+        ) : (
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {reservations.map( (r) => {
+                        <tr key={r.rno}>
+                            <td></td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        )}
         </>
     )
 }// func end
