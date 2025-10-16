d943a4ef16f7c7c35e8c131f6f0147734ee61712
LEE-GYEO-RE <rufp9891@gmail.com>
2025-10-16 16:58:58 +0900

    20251016_Header.jsx_css 파일 분리

--- src/main/reactapp/src/components/Header.jsx
+++ src/main/reactapp/src/components/Header.jsx
@@ -1,37 +1,50 @@
 import React from "react";
 import { AppBar, Toolbar, Box, Typography, Button } from "@mui/material";
-import styles from "./Header.module.css";
+import styles from "../styles/Header.module.css";
+import { useNavigate } from "react-router-dom";
 
-const Header = () =>  {
-  return(
-    <AppBar position="static" className={styles.appBar}>
+const Header = () => {
+
+  const navigate = useNavigate();
+
+  return (
+    <AppBar position="relative" className={styles.appBar}>
       <Toolbar className={styles.toolbar}>
         {/* 로고 */}
-        <Typography variant="h6" className={styles.logo}>
+        <Typography variant="h6" className={styles.logo}
+          onClick= {() => navigate("/")}
+        >
           ⚾ PHOENIX
-          </Typography>
-          {/* 네비게이션 메뉴 */}
-          <Box className={styles.nav}>
-            {["TICKET", "PLAYERS", "GAME", "CONTENTS", "MEMBERSHIP"].map(
-              (menu) => (
-                <Button key={menu} className={styles.navButton}>
-                  {menu}
-                </Button>
-              )
-            )}
-          </Box>
-
-            {/* 로그인/회원가입 버튼 */}
-            <Box className={styles.auth}>
-              <Button variant="outlined" className={styles.loginBtn}>
-                로그인
+        </Typography>
+        {/* 네비게이션 메뉴 */}
+        <Box className={styles.nav}>
+          {["TICKET", "PLAYERS", "GAME", "CONTENTS", "MEMBERSHIP"].map(
+            (menu) => (
+              <Button key={menu} className={styles.navButton}>
+                {menu}
               </Button>
-              
-              <Button variant="contained" className={styles.signupBtn}>
-                회원가입
-              </Button>
+            )
+          )}
+        </Box>
 
-            </Box>
+        {/* 로그인/회원가입 버튼 */}
+        <Box className={styles.auth}>
+          <Button
+            variant="outlined"
+            className={styles.loginBtn}
+            onClick={() => navigate("/login")}
+          >
+            로그인
+          </Button>
+          <Button
+            variant="contained"
+            className={styles.signupBtn}
+            onClick={() => navigate("/signup")}
+          >
+            회원가입
+          </Button>
+
+        </Box>
 
       </Toolbar>
     </AppBar>
