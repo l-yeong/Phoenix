5335e9bcd001ff3aec27867ec80daf2bb98ee39d
l-yeong <yahn1204@gmail.com>
2025-10-20 18:09:49 +0900

    251020_main.jsx_필요없는것제거

--- src/main/reactapp/src/main.jsx
+++ src/main/reactapp/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
-import TicketQR from "./features/tickets/TicketQR.jsx";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -10,10 +10,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-
-// // // QR 이미지테스트 용도
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <TicketQR />
-//   </React.StrictMode>
-// );
